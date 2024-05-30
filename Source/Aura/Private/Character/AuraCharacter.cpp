13de1b6d0ae2ce81f78703d731d36c576a8544be
RSkala <RichardSkala@gmail.com>
2024-05-29 23:22:40 -0700

    First pass character movement implementation

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -3,3 +3,28 @@
 
 #include "Character/AuraCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
+AAuraCharacter::AAuraCharacter()
+{
+	// Get the Character Movement Component from this Character
+	UCharacterMovementComponent* CharacterMovementComponent = GetCharacterMovement();
+	check(CharacterMovementComponent);
+
+	// Orient the player character's direction to the movement direction
+	CharacterMovementComponent->bOrientRotationToMovement = true;
+
+	// Set the rotation movement rate
+	CharacterMovementComponent->RotationRate = FRotator(0.0f, 400.0f, 0.0f); // This should be configurable in the Blueprint, but give it a default value
+
+	// Constrain movement to a plane
+	CharacterMovementComponent->bConstrainToPlane = true;
+
+	// Snap the player character to a plane
+	CharacterMovementComponent->bSnapToPlaneAtStart = true;
+
+	// The character itself should not use the controller's rotation
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+}
