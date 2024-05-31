ba8a96d09b1a15af09727e4d5e8ad7cf09dfcf4a
RSkala <RichardSkala@gmail.com>
2024-05-30 17:01:48 -0700

    Created IEnemyInterface

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -3,3 +3,14 @@
 
 #include "Character/AuraEnemy.h"
 
+DEFINE_LOG_CATEGORY_STATIC(LogAuraEnemy, Log, All)
+
+void AAuraEnemy::HighlightActor()
+{
+	UE_LOG(LogAuraEnemy, Log, TEXT("AAuraEnemy::HighlightActor - %s"), *GetName());
+}
+
+void AAuraEnemy::UnhighlightActor()
+{
+	UE_LOG(LogAuraEnemy, Log, TEXT("AAuraEnemy::UnhighlightActor - %s"), *GetName());
+}
