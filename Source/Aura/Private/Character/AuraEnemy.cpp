4ceee6ee417ea0f800e81e29a26a216b0015ad4d
RSkala <RichardSkala@gmail.com>
2024-05-31 15:59:42 -0700

    Enable highlighting on the enemies. Disabled debug sphere on highlight in Event Tick in BP_EnemyBase

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -1,6 +1,7 @@
 // Copyright Richard Skala
 
 
+#include "Aura/Aura.h"
 #include "Character/AuraEnemy.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogAuraEnemy, Log, All)
@@ -9,10 +10,36 @@ void AAuraEnemy::HighlightActor()
 {
 	UE_LOG(LogAuraEnemy, Log, TEXT("AAuraEnemy::HighlightActor - %s"), *GetName());
 	bHighlighted = true;
+
+	// Highlight the enemy
+	if (USkeletalMeshComponent* EnemySkeletalMesh = GetMesh())
+	{
+		EnemySkeletalMesh->SetRenderCustomDepth(true);
+		EnemySkeletalMesh->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	}
+
+	// Highlight the enemy weapon
+	if (Weapon != nullptr)
+	{
+		Weapon->SetRenderCustomDepth(true);
+		Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	}
 }
 
 void AAuraEnemy::UnhighlightActor()
 {
 	UE_LOG(LogAuraEnemy, Log, TEXT("AAuraEnemy::UnhighlightActor - %s"), *GetName());
 	bHighlighted = false;
+
+	// Unhighlight the enemy
+	if (USkeletalMeshComponent* EnemySkeletalMesh = GetMesh())
+	{
+		EnemySkeletalMesh->SetRenderCustomDepth(false);
+	}
+
+	// Unhighlight the enemy weapon
+	if (Weapon != nullptr)
+	{
+		Weapon->SetRenderCustomDepth(false);
+	}
 }
