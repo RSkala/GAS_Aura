15e0a46cec3b7e807fcddc9ce793300cd02f4f15
RSkala <RichardSkala@gmail.com>
2024-06-02 14:23:20 -0700

    Created C++ and Blueprint PlayerState. Set Player State class in Game Mode

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,10 @@
+// Copyright Richard Skala
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	// How often per second the server will try to update clients
+	NetUpdateFrequency = 100.0f;
+}
